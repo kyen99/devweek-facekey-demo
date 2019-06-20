e371774fb3d2d98f4b3ae01cdfc7c96e90c6a11b
Kai Yen <kai@purplesquirrel.io>
2019-06-19 20:56:28 -0400

    Wrapped up javascript

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # Dev Week Face Key Demo
 
+## TODO
+
+1. Create python server that opens a websocket and listens and responds to messages as described below.
+2. Update app/facekey.js with correct server ip and port
+
+## Websocket communications
+
+Here is the flow for the websocket communications:
+
+- Start server listening for websocket connections
+- Load client web page in browser, websocket connection is opened
+- Click "Detect Face" button and it sends to server:
+
+`{ "action": "checkFace" }`
+
+- Server takes photo checks for a face and returns either:
+
+`{ "action": "checkFace", "payload": true }` if there is a face or
+
+`{ "action": "checkFace", "payload": false }` is there is no face
+
+- If true, checkmark goes green, if false an alert pops
+- Insert Yubikey and touch contact, client sends:
+
+`{ "action": "authenticate", "payload": "[YUBIKEY OTP VALUE HERE]" }`
+
+- Server validates key and sends either:
+
+`{ "action": "authenticate", "payload": true }` if key passes
+
+`{ "action": "authenticate", "payload": false}` if key fails
+
+- If key passes "You're in" message, or alert warning.
+
+## Notes
+
+- Before touching the Yubikey to send the OTP code, make sure you click somewhere in the browser window to make sure it has focus. Otherwise the OTP code will get sent to whatever has the focus.
+- You can simulate a bad Yubikey by simply typing a few characters and pressing <ENTER>.
--- app/facekey.js
+++ app/facekey.js
@@ -1,33 +1,71 @@
-const server = 'wss://echo.websocket.org/'
+// Configure these stuff
+const server = 'ws://echo.websocket.org'
+const port = 80
 
-const ws = new WebSocket(server)
+// Open websocket connection
+const ws = new WebSocket(`${server}:${port}`)
+
+// Global var for storing keystrokes from yubikey
 var keys = ''
 
+// Listen for websocket open event
 ws.onopen = function(e) {
-  console.log('connected')
+  console.log('connected to server')
 }
+
+// Listen for websocket close event
 ws.onclose = function(e) {
-  console.log(e)
+  console.log('server disconnected')
 }
-ws.onmessage = function(e) {
-  if (e.data === 'face detected') {
-    document.getElementById('checkOne').classList.add('fas-ready')
+
+// Listen for messages received
+ws.onmessage = receiveMessage
+
+// Listen for OTP keys sent
+window.onkeypress = receiveOtp
+
+// Listen for a click on the "Detect Face" button
+document.getElementById('checkFace').addEventListener('click', function() {
+  sendMessage({
+    action: 'checkFace',
+  })
+})
+
+// Handler for incoming websocket messages
+function receiveMessage(e) {
+  const message = JSON.parse(e.data)
+  console.log('Message received:')
+  console.log(e)
+  if (message.action === 'checkFace') {
+    if (message.payload === true) {
+      document.getElementById('checkOne').classList.add('fas-ready')
+    } else [alert('No face detected.')]
   }
-  if (e.data === 'authenticated') {
-    document.getElementById('loggedIn').classList.add('show')
-    document.getElementById('notLoggedIn').classList.add('hide')
+  if (message.action === 'authenticate') {
+    if (message.payload === true) {
+      document.getElementById('loggedIn').classList.add('show')
+      document.getElementById('notLoggedIn').classList.add('hide')
+    } else {
+      alert('Yubikey authentication failed.')
+    }
   }
 }
 
-function sendMessage(message) {
-  console.log('trying to send... ' + message)
+// Send websocket message
+function sendMessage(m) {
+  const message = JSON.stringify(m)
+  console.log('Sending... ' + message)
   ws.send(message)
 }
 
-window.onkeypress = function(e) {
+// Handler for receiving keyboard input
+function receiveOtp(e) {
   if (e.key === 'Enter') {
     console.log('got otp: ' + keys)
-    document.getElementById('checkTwo').classList.add('fas-ready')
+    sendMessage({
+      action: 'authenticate',
+      payload: keys,
+    })
     keys = ''
   } else {
     keys = keys + e.key
